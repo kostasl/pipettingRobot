df3ee413c330a102b203f6e40ea763f5586eda1e
kostasl <costaslag@gmail.com>
2016-06-27 22:24:20 +0100

    Split files to organize - tested SD card Support - List Files

--- src/lh_controller/SD_storage.ino
+++ src/lh_controller/SD_storage.ino
@@ -47,5 +47,41 @@ void printDirectory(File dir, int numTabs) {
   }
 }
 
+void testReadWrite()
+{
+  // open the file. note that only one file can be open at a time,
+    // so you have to close this one before opening another.
+    myFile = SD.open("test.txt", FILE_WRITE);
+  
+    // if the file opened okay, write to it:
+    if (myFile) {
+      Serial.print("Writing to test.txt...");
+      myFile.println("testing 1, 2, 3.");
+      // close the file:
+      myFile.close();
+      Serial.println("done.");
+    } else {
+      // if the file didn't open, print an error:
+      Serial.println("error opening test.txt");
+    }
+  
+    // re-open the file for reading:
+    myFile = SD.open("test.txt");
+    if (myFile) {
+      Serial.println("test.txt:");
+  
+      // read from the file until there's nothing else in it:
+      while (myFile.available()) {
+        Serial.write(myFile.read());
+      }
+      // close the file:
+      myFile.close();
+    } else {
+      // if the file didn't open, print an error:
+      Serial.println("error opening test.txt");
+    }
+  }
+
+}
 
 
